a7e9ea979e4762c7a27263325b84fe111d95d96f
Brian Peiris <brianpeiris@gmail.com>
2018-03-22 08:00:32 -0700

    final script tweaks?

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,18 +1,20 @@
 npm install && 
-NODE_ENV=production npm run build && 
+npm run build -- --mode=production && 
 
 cd demo && 
 npm install &&
-NODE_ENV=production npm run build &&
+npm run build -- --mode=production &&
 cd .. &&
 
 echo "" &&
-echo "Build complete. Do some testing." &&
+echo "Build complete. Do some testing. Ctrl-C when you're done." &&
 echo "" &&
 
 trap '' INT
-serve &
-wait %1
+serve
+
+echo "Press enter to release or Ctrl-C to abort."
+read _
 
 git commit -am 'update builds' &&
 git push &&
